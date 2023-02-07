ed05b7b386360e7fc039026ad5e112495059d91a
My Name <kiykobogdan@gmail.com>
2023-02-07 13:33:21 +0200

    Add project files.

---
+++ Classbeg/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Классы в.Net.Класс - инструмент для создания ссылочных типов в .Net.
+    Класс по умолчанию имеет модификатор  internal (доступ только в текущем проекте-сборке).
+    Поля класса, методы класса по умолчанию приватные как и в c++.
+Наш тип от родителя object получает набор готовых методов среди которых есть ToString
+Данный метод выполняет пребразование нашего типа к строке.Для переопределения используется
+ключевое слово override*/
+
+namespace Classbeg
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* City a = new City("Ivan",20);//Stud *a;*/
+            City city1 = new City();
+            city1.NameCity = "Kiev";
+            city1.NameCountry = "Ukraine";
+            city1.NameRegion = "Kievska obl";
+            city1.People = 4500000;
+            city1.Phone = 380;
+            Console.Write(city1);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classbeg/Stud.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classbeg
+{
+    public class City
+    {
+        private string namecity;
+        private string namecountry;
+        private int summofpeople;
+        private int phonenumber;
+        private string namecityragion;
+        public string NameCity
+        {
+            get { return namecity; }
+
+            set { namecity = value; }
+        }
+        public string NameCountry
+        {
+            get { return namecountry; }
+
+            set { namecountry = value; }
+        }
+        public int People
+        {
+            get { return summofpeople; }
+
+            set { summofpeople = value; }
+        }
+        public int Phone
+        {
+            get { return phonenumber; }
+
+            set { phonenumber = value; }
+        }
+        public string NameRegion
+        {
+            get { return namecityragion; }
+
+            set { namecityragion = value; }
+        }
+
+        public City() {
+            namecity = "Null";
+            namecityragion = "Null";
+            namecountry = "Null";
+            phonenumber = 0;
+            phonenumber = 0;
+        }
+        public City(string namecity, string namecountry, int summofpeople,int phonenumber, string namecityragion)
+        {
+            this.namecity = namecity;
+            this.namecountry = namecountry;
+            this.summofpeople = summofpeople;
+            this.phonenumber = phonenumber;
+            this.namecityragion = namecityragion;
+        }
+        public void Print()
+        {
+           
+        }
+        public override string ToString()
+        {
+            return $"Name City:{namecity}\nName Country:{namecountry}\nPeople who live:{summofpeople}\nPhone number:{phonenumber}\nName city region:{namecityragion}";// переопределение ToString
+        }
+       
+    }
+}
